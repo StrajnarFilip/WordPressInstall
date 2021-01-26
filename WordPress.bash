776a6b8e001bcacc9dcfee46c45151b1d384703b
StrajnarFilip <46705237+StrajnarFilip@users.noreply.github.com>
2021-01-26 23:27:12 +0100

    Update WordPress.bash

--- WordPress.bash
+++ WordPress.bash
@@ -1,3 +1,19 @@
+    # WordPressInstall, a simple WordPress installation script
+    # Copyright (C) 2021  Filip Strajnar
+    
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #!/bin/bash
 if ! command -v apt &> /dev/null
 then
